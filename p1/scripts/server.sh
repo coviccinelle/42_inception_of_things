4ebb54f207a177329c6b73dea09b2e826a3b7733
thi-phng <coviccinelle@gmail.com>
2026-06-02 14:01:21 +0000

    done script for Server

---
+++ p1/scripts/server.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+echo "Setting up K3s Server (Controller) ..."
+
+export K3S_KUBECONFIG_MODE="644"
+export K3S_TOKEN="IoT-Secret-Token-42"
+
+# add some flags but eth1 (let k3s auto-recongnize)
+
+export INSTALL_K3S_EXEC="server \
+  --bind-address=192.168.56.110 \
+  --advertise-address=192.168.56.110 \
+  --node-ip=192.168.56.110"
+
+curl -sfL https://get.k3s.io | sh -
+
+echo "==> wait for K3s ... "
+sleep 10
+echo "==== Done installation K3s Server! ===="
